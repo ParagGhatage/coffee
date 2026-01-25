5362b002df2ce981cb5d8804bed380ae2c30c058
Parag Ghatage <parag.dev2025@gmail.com>
2026-01-26 01:10:14 +0530

    Horizontal bar is rendering in real time

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"time"
 	tea "github.com/charmbracelet/bubbletea"
 	cpu "github.com/shirou/gopsutil/v4/cpu"
+	progress_bar "github.com/charmbracelet/bubbles/progress"
+	lip "github.com/charmbracelet/lipgloss"
 )
 
 //  CPU
@@ -19,9 +21,22 @@ func tickCpu() tea.Msg {
 	return cpuMsg(percent_usage)
 }
 
+// PROGRESS BAR for current CPU usage
+const (
+	padding  = 0
+	maxWidth = 250
+)
+
+var helpStyle = lip.NewStyle().Foreground(lip.Color("#626262")).Render
+
+
+
+
 // MODEL
 type model struct{
 	cpu_usage float64
+	
+	progress progress_bar.Model
 }
 
 // Init
@@ -32,33 +47,65 @@ func (m model) Init() tea.Cmd{
 }
 
 // UPDATE
-func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd){
-
-	switch msg :=msg.(type){
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+    
 	case tea.KeyMsg:
-		if msg.String() =="q" || msg.Type == tea.KeyCtrlC{
-			return m,tea.Quit
+		if msg.String() == "q" || msg.Type == tea.KeyCtrlC {
+			return m, tea.Quit
 		}
 
 	case cpuMsg:
-		if len(msg)>0{
-			m.cpu_usage =msg[0]
+		if len(msg) > 0 {
+			m.cpu_usage = msg[0]
+			// Update the progress bar to the new percentage
+			cmd := m.progress.SetPercent(m.cpu_usage / 100.0)
+			
+			// Batch: Run the bar animation AND the next CPU tick
+			return m, tea.Batch(tickCpu, cmd)
 		}
-		return m,tickCpu
+		return m, tickCpu
 
+	case tea.WindowSizeMsg:
+		m.progress.Width = msg.Width - padding*2 - 4
+		if m.progress.Width > maxWidth {
+			m.progress.Width = maxWidth
+		}
+		return m, nil
+
+	// CRITICAL ADDITION: This allows the progress bar to animate
+	case progress_bar.FrameMsg:
+		progressModel, cmd := m.progress.Update(msg)
+		m.progress = progressModel.(progress_bar.Model)
+		return m, cmd
 	}
-	return m,nil
-}
 
+	return m, nil
+}
 // VIEW
 func (m model) View() string {
 	
-	return fmt.Sprintf("Current CPU usage:%2f\n\nPress '+' to increment.\nPress 'q' to quit.\n",m.cpu_usage )
+	pad := lip.NewStyle().Padding(2).Render
+	
+	// rendering the progress bar view
+	return pad(fmt.Sprintf(
+		"CPU Usage: %.2f%%\n\n%s\n\nPress 'q' to quit.", 
+		m.cpu_usage, 
+		m.progress.View(), // <-- This prints the bar
+	))
 }
 
 // MAIN
 func main(){
-	p := tea.NewProgram(model{})
+	
+	prog := progress_bar.New(progress_bar.WithScaledGradient("#FF7CCB", "#FDFF8C"))
+
+	intialModel := model{
+		cpu_usage: 0,
+		progress: prog,
+	}
+
+	p := tea.NewProgram(intialModel)
 
 	if _,err := p.Run(); err != nil {
 	fmt.Printf("There's an error: %v" ,err)
