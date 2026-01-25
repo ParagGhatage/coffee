829e3abdac99ac6ec46cb61f865cc841cc0aff4c
Parag Ghatage <parag.dev2025@gmail.com>
2026-01-26 00:19:11 +0530

    Cpu usage are being rendered every 650 Milliseconds

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 
 TUI for visualising system stats.
+
+ Goal is to have all the features of `htop` but with better UI and some extra features.
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"time"
 	tea "github.com/charmbracelet/bubbletea"
 	cpu "github.com/shirou/gopsutil/v4/cpu"
 )
@@ -11,6 +12,12 @@ import (
 // custom message to carry cpu data
 type cpuMsg [] float64
 
+//Function to calculate cpu usage and return as a message when done
+// 1sec  = 1000 Milliseconds
+func tickCpu() tea.Msg {
+	percent_usage , _ := cpu.Percent(650*time.Millisecond,false)
+	return cpuMsg(percent_usage)
+}
 
 // MODEL
 type model struct{
@@ -18,8 +25,10 @@ type model struct{
 }
 
 // Init
+// tea.Cmd is just defined type for function- i.e. Init should return a fucntion that it will run at the start of TUI
 func (m model) Init() tea.Cmd{
-	return nil
+
+	return tickCpu
 }
 
 // UPDATE
@@ -31,16 +40,23 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd){
 			return m,tea.Quit
 		}
 
-		
+	case cpuMsg:
+		if len(msg)>0{
+			m.cpu_usage =msg[0]
+		}
+		return m,tickCpu
+
 	}
 	return m,nil
 }
 
+// VIEW
 func (m model) View() string {
-	percent , _ := cpu.Percent(0,false)
-	return fmt.Sprintf("The cpu USAGE is:%2f\n\nPress '+' to increment.\nPress 'q' to quit.\n",percent[0] )
+	
+	return fmt.Sprintf("Current CPU usage:%2f\n\nPress '+' to increment.\nPress 'q' to quit.\n",m.cpu_usage )
 }
 
+// MAIN
 func main(){
 	p := tea.NewProgram(model{})
 
